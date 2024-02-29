7ae59d988352f6779db549c15bbf5bf7abfa2fa8
Justine397 <150914796+Justine397@users.noreply.github.com>
2024-02-29 14:38:47 +0800

    Add files via upload

---
+++ colorScript.js
@@ -0,0 +1,15 @@
+function redBG(){
+    document.body.style.background = 'red';
+    setTimeout(BackToOG, 2000);
+}
+function greenBG(){
+    document.body.style.background = 'green';
+    setTimeout(BackToOG, 2000);
+}
+function blueBG(){
+    document.body.style.background = "blue";
+    setTimeout(BackToOG, 2000);
+}
+function BackToOG(){
+    document.body.style.background = 'yellow';
+}
