4a43a281571d982b19129cb75053a20283c8a83d
Justine397 <150914796+Justine397@users.noreply.github.com>
2024-03-14 14:26:15 +0800

    all bootstrap

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <title>FrontEnd Bootcamp</title>
+</head>
+<body>
+
+    <!-- NAVBAR -->
+
+    <nav class="navbar navbar-expand-lg bg-light navbar-light py-3">
+        <div class="container">
+            <a href="#" class="navbar-brand">
+            <img class="img-fluid rounded" src="img/img2/small.jpg" alt="" width="40" height="34">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
+                <span class="navbar-toggler-icon">
+                </span>
+            </button>
+                <div class="collapse navbar-collapse" id="navmenu">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a href="#text1" class="nav-link"> text1 </a>
+                        </li>
+                        <li class="nav-item">   
+                            <a href="#text2" class="nav-link"> text2 </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#text3" class="nav-link"> text3 </a>
+                        </li>
+                    </ul>
+                </div>
+        </div>
+    </nav>
+
+    <!--MainContent-->
+
+    <section class="bg-light text-dark p-5 p-lg-0 pt-lg-3 text-center">
+        <div class="container">
+            <div class="row p-2">
+                <div class="col-7 pe-0">
+                    <div class="text-warning">
+                        TEXT
+                    </div>    
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rhoncus pulvinar pharetra. Aenean tristique lobortis diam, sed convallis magna feugiat justo sit amet. In nec scelerisque justo. Fusce laoreet ipsum a tellus semper, nec interdum nibh malesuada. Morbi egestas sem a ex vehicula, id tempus ligula molestie. Suspendisse potenti. Duis et odio at magna pellentesque congue at dapibus velit. Vivamus vel nisi orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sed magna eget purus pulvinar placerat. In a nisl vel justo tincidunt sagittis quis nec lectus. Cras consequat vitae sapien et finibus. Nullam erat ante, finibus sit amet feugiat vel, sollicitudin a ipsum. Praesent laoreet elementum est, fringilla facilisis metus sollicitudin sed. Etiam a interdum nunc. Vivamus aliquet mollis commodo. In tincidunt, tellus sit amet suscipit scelerisque, ligula leo pulvinar sem, sed mattis erat nisi nec eros. Nulla finibus ante felis, non tincidunt risus posuere et. Vivamus dignissim interdum euismod. Integer a dignissim metus, ac faucibus urna. Phasellus tincidunt pulvinar arcu id hendrerit. Maecenas molestie sollicitudin mi, in cursus tortor elementum et.
+                </div>
+                
+                <!-- <div class="vr g-0" ></div> -->
+                <div class="col-5">
+                    <div class="col p-3">
+                        <div class="text-success mb-3">
+                            TEXT
+                        </div>
+                        <img class="img-fluid rounded" src="img/img2/small.jpg" alt="" width="100" height="100">
+                    </div>
+                    <!-- <div class="hr g-0"></div> -->
+                    <div class="col p-3">
+                        <div class="text-danger mb-3">
+                            TEXT
+                        </div>
+                        <img class="img-fluid rounded" src="img/img2/small.jpg" alt="" width="100" height="100">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--FOOTER-->
+
+    <section class="bg-light text-dark p-2">
+        <div class="container">
+            <div class="bg-light text-dark p-3 p-lg-0 pt-lg-3 text-center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rhoncus pulvinar pharetra. Aenean tristique lobortis diam, sed convallis magna feugiat justo sit amet. In nec scelerisque justo. Fusce laoreet ipsum a tellus semper, nec interdum nibh malesuada. Morbi egestas sem a ex vehicula, id tempus ligula molestie.
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
+
+<!--border-1 border-start border-black-->
